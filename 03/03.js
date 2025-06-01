31a8fe83ce8073788dc68a89096356ea04785b32
Jinwon Choi <jjwls97@catholic.ac.kr>
2025-06-01 14:21:48 +0900

    lesson03

---
+++ 03/03.js
@@ -0,0 +1,39 @@
+let date = new Date();
+switch (date.getMonth() + 1) {
+    case 1:
+        console.log("1월 입니다.");
+        break;
+    case 2:
+        console.log("2월 입니다.");
+        break;
+    case 3:
+        console.log("3월 입니다.");
+        break;
+    case 4:
+        console.log("4월 입니다.");
+        break;
+    case 5:
+        console.log("5월 입니다.");
+        break;
+    case 6:
+        console.log("6월 입니다.");
+        break;
+    case 7:
+        console.log("7월 입니다.");
+        break;
+    case 8:
+        console.log("8월 입니다.");
+        break;
+    case 9:
+        console.log("9월 입니다.");
+        break;
+    case 10:
+        console.log("10월 입니다.");
+        break;
+    case 11:
+        console.log("11월 입니다.");
+        break;
+    case 12:
+        console.log("12월 입니다.");
+        break;
+}
